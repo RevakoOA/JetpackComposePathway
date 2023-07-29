822ba61cc0a53bd6ebbf9427c2d2266ed9db7d29
Ostap <revakooa@gmail.com>
2023-07-29 14:11:53 +0300

    1.4 Step 5 - Reusing composables.
     - Add MyApp as a main view of the activity. Used in both Activity and preview.
     - [Step link](https://developer.android.com/codelabs/jetpack-compose-basics#4).

--- 1_4_basicscodelab/src/main/java/com/ostapr/a1_4_basicscodelab/MainActivity.kt
+++ 1_4_basicscodelab/src/main/java/com/ostapr/a1_4_basicscodelab/MainActivity.kt
@@ -20,15 +20,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicsCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Greeting("Android")
     }
 }
 
@@ -46,6 +50,6 @@ fun Greeting(name: String) {
 @Composable
 fun GreetingPreview() {
     BasicsCodelabTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
