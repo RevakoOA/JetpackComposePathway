f15e1a3ae839795f1406d428d1bf63772d4bb26f
Ostap <revakooa@gmail.com>
2023-07-31 23:22:40 +0300

    1.7 Step 4 - Events in compose.
     - Added the button that should change the state, currently not working.
     - [Step link](https://developer.android.com/codelabs/jetpack-compose-state#3).

--- 1_7_basic_state/src/main/java/com/ostapr/a1_7_basic_state/MainActivity.kt
+++ 1_7_basic_state/src/main/java/com/ostapr/a1_7_basic_state/MainActivity.kt
@@ -3,13 +3,16 @@ package com.ostapr.a1_7_basic_state
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,11 +42,16 @@ private fun WellnessScreen(modifier: Modifier = Modifier) {
 
 @Composable
 private fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
-        text = "You've had $count glasses.",
-        modifier = Modifier.padding(16.dp)
-    )
+    var count = 0
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = "You've had $count glasses.",
+            modifier = Modifier.padding(16.dp)
+        )
+        Button(onClick = { count += 1 }, modifier = Modifier.padding(top = 8.dp)) {
+            Text("Add one")
+        }
+    }
 }
 
 @Preview(showBackground = true)
