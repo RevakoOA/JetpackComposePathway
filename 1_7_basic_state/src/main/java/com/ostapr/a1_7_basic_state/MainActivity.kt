9dc3fabaca9d588f4346c0e0916e4faf9e4df47a
Ostap <revakooa@gmail.com>
2023-07-31 23:55:59 +0300

    1.7 Step 7 - Remember in Composition. Part 1.
     - State is forgotten once it's removed from composition.
     - [Step link](https://developer.android.com/codelabs/jetpack-compose-state#7).

--- 1_7_basic_state/src/main/java/com/ostapr/a1_7_basic_state/MainActivity.kt
+++ 1_7_basic_state/src/main/java/com/ostapr/a1_7_basic_state/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.ostapr.a1_7_basic_state
 
+import WellnessTaskItem
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -47,16 +51,33 @@ private fun WellnessScreen(modifier: Modifier = Modifier) {
 private fun WaterCounter(modifier: Modifier = Modifier) {
     var count by remember { mutableStateOf(0) }
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(
-            text = "You've had $count glasses.",
-            modifier = Modifier.padding(16.dp)
-        )
-        Button(
-            onClick = { count += 1 },
-            enabled = count < 10,
-            modifier = Modifier.padding(top = 8.dp)
-        ) {
-            Text("Add one")
+        if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    taskName = "Have you taken your 15 mins walk today?",
+                    onClose = { showTask = false })
+            }
+            Text(
+                text = "You've had $count glasses.",
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+        Row() {
+            Button(
+                onClick = { count += 1 },
+                enabled = count < 10,
+                modifier = Modifier.padding(top = 8.dp)
+            ) {
+                Text("Add one")
+            }
+            Spacer(Modifier.size(10.dp))
+            Button(
+                onClick = { count = 0 },
+                modifier = Modifier.padding(top = 8.dp)
+            ) {
+                Text("Clear water count")
+            }
         }
     }
 }
