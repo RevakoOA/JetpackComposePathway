b34e0754e71a350c47f690b038457858604ea67e
Ostap <revakooa@gmail.com>
2023-07-31 23:30:54 +0300

    1.7 Step 5 - Memory in a composable function.
     - State is fixed. Button is working now.
     - [Step link](https://developer.android.com/codelabs/jetpack-compose-state#4).

--- 1_7_basic_state/src/main/java/com/ostapr/a1_7_basic_state/MainActivity.kt
+++ 1_7_basic_state/src/main/java/com/ostapr/a1_7_basic_state/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,6 +14,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,10 +43,9 @@ private fun WellnessScreen(modifier: Modifier = Modifier) {
     }
 }
 
-
 @Composable
 private fun WaterCounter(modifier: Modifier = Modifier) {
-    var count = 0
+    var count by remember { mutableStateOf(0) }
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
         Text(
             text = "You've had $count glasses.",
