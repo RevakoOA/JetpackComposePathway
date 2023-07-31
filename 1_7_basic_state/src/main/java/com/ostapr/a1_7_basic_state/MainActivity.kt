4a9037070dcd33c0f5e9a866b4265d993e1eb814
Ostap <revakooa@gmail.com>
2023-07-31 23:59:18 +0300

    1.7 Step 8 - Restore state in Composable.
     - Switched from remember to rememberSaveable tp outlive config changes.
     - [Step link](https://developer.android.com/codelabs/jetpack-compose-state#7).

--- 1_7_basic_state/src/main/java/com/ostapr/a1_7_basic_state/MainActivity.kt
+++ 1_7_basic_state/src/main/java/com/ostapr/a1_7_basic_state/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -49,7 +50,7 @@ private fun WellnessScreen(modifier: Modifier = Modifier) {
 
 @Composable
 private fun WaterCounter(modifier: Modifier = Modifier) {
-    var count by remember { mutableStateOf(0) }
+    var count by rememberSaveable { mutableStateOf(0) }
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
         if (count > 0) {
             Text(
