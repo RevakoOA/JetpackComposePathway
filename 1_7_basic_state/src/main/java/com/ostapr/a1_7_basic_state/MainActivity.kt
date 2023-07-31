49e8d5e51b3261f285e42e90d31c8440a0705ebb
Ostap <revakooa@gmail.com>
2023-08-01 00:28:37 +0300

    1.7 Step 9 - State hoisting.
     - State was lifted to statefull part. The major part was remained in stateless part.
     - [Step link](https://developer.android.com/codelabs/jetpack-compose-state#8).

--- 1_7_basic_state/src/main/java/com/ostapr/a1_7_basic_state/MainActivity.kt
+++ 1_7_basic_state/src/main/java/com/ostapr/a1_7_basic_state/MainActivity.kt
@@ -1,26 +1,20 @@
 package com.ostapr.a1_7_basic_state
 
-import WellnessTaskItem
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,13 +38,24 @@ private fun WellnessScreen(modifier: Modifier = Modifier) {
         modifier = modifier.fillMaxWidth(),
         color = MaterialTheme.colorScheme.background
     ) {
-        WaterCounter()
+        StatefullWaterCounter()
     }
 }
 
 @Composable
-private fun WaterCounter(modifier: Modifier = Modifier) {
+private fun StatefullWaterCounter(modifier: Modifier = Modifier) {
     var count by rememberSaveable { mutableStateOf(0) }
+    StatelessWaterCounter(count, onIncrease = {
+        count += 1
+    }, modifier = modifier)
+}
+
+@Composable
+private fun StatelessWaterCounter(
+    count: Int,
+    onIncrease: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
         if (count > 0) {
             Text(
@@ -59,7 +64,7 @@ private fun WaterCounter(modifier: Modifier = Modifier) {
             )
         }
         Button(
-            onClick = { count += 1 },
+            onClick = onIncrease,
             enabled = count < 10,
             modifier = Modifier.padding(top = 8.dp)
         ) {
@@ -72,6 +77,6 @@ private fun WaterCounter(modifier: Modifier = Modifier) {
 @Composable
 private fun WaterCounterPreview() {
     BasicStateTheme {
-        WaterCounter()
+        StatefullWaterCounter()
     }
 }
