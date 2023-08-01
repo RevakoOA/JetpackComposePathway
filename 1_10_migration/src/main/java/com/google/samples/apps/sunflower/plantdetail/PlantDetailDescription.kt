414f6515b38fff531ebf4e1f7ca680f657940ac6
Ostap <revakooa@gmail.com>
2023-08-01 18:36:38 +0300

    1.10 Step 6 - Add plant name.
     - TextView `plant_detail_name` converted to  Plant Name composable.
     - [Step link](https://developer.android.com/codelabs/jetpack-compose-migration#5).

--- 1_10_migration/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ 1_10_migration/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,19 +16,41 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.samples.apps.sunflower.R
+
+@Composable
+fun PlantName(name: String) {
+    Text(
+        name,
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
+}
+
 
 @Composable
 fun PlantDetailDescription() {
     Surface {
-        Text("Hello Compose")
+        PlantName("Apple")
     }
 }
 
-@Preview
+@Preview(widthDp = 320)
 @Composable
 fun PlantDetailDescriptionPreview() {
     PlantDetailDescription()
