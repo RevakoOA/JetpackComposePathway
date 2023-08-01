7f4bcee1b6a59742a7ba7a204947c558cb35ce49
Ostap <revakooa@gmail.com>
2023-08-01 21:20:46 +0300

    1.10 Step 9 - Views in compose.
     - As for description we need to show spanned text - use AndroidView factory to create regular TextView.
     - [Step link](https://developer.android.com/codelabs/jetpack-compose-migration#8).

--- 1_10_migration/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ 1_10_migration/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,8 +16,11 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.MaterialTheme
@@ -26,6 +29,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
@@ -33,6 +37,8 @@ import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
@@ -70,11 +76,35 @@ fun PlantWatering(wateringInterval: Int) {
 }
 
 @Composable
+fun PlantDescription(description: String) {
+    val htmlDescription = remember(description) {
+        HtmlCompat.fromHtml(description, HtmlCompat.FROM_HTML_MODE_COMPACT)
+    }
+
+    AndroidView(
+        factory = { context ->
+            TextView(context).apply {
+                movementMethod = LinkMovementMethod.getInstance()
+            }
+        },
+        update = {
+            it.text = htmlDescription
+        },
+        modifier = Modifier
+            .padding(top = dimensionResource(id = R.dimen.margin_small))
+            .heightIn(
+                dimensionResource(R.dimen.plant_description_min_height)
+            ),
+    )
+}
+
+@Composable
 fun PlantDetailContent(plant: Plant) {
     Surface {
         Column(Modifier.padding(dimensionResource(R.dimen.margin_normal))) {
             PlantName(name = plant.name)
             PlantWatering(plant.wateringInterval)
+            PlantDescription(plant.description)
         }
     }
 }
@@ -91,6 +121,6 @@ fun PlantDetailDescription(viewModel: PlantDetailViewModel) {
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun PlantDetailContentPreview() {
-    val plant = Plant("id", "Apple", "Description", 3, 30, "")
+    val plant = Plant("id", "Apple", "HTML<br><br>description", 3, 30, "")
     PlantDetailContent(plant)
 }
